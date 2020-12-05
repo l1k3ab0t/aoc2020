f3f8e06a6224e851d6d78f6c1b1998e6c9c8b7d9
l1k3ab0t <thiesfronius@gmail.com>
2020-12-05 12:38:48 +0100

    golfing complete.

--- day05/day5.py
+++ day05/day5.py
@@ -1,2 +1,2 @@
-s=sorted(map(lambda x:int(x[:7],2)*8+int(x[7:-1],2),(x.translate({66:49,70:48,82:49,76:48}) for x in open("input").readlines())))
+s=sorted(int(x[:-1].translate({66:49,70:48,82:49,76:48}),2)for x in open("input").readlines())
 print(s[-1], sum(range(s[0],s[-1]+1))-sum(s)) 
